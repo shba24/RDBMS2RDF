7562e671e4fbfaf7356a35e97b7f0c824b2e70d2
Rahul Kumar <rahulrjjaiswal07@gmail.com>
2022-02-15 16:37:09 -0700

    Added javadoc comments.

--- javaminibase/src/global/LID.java
+++ javaminibase/src/global/LID.java
@@ -7,12 +7,20 @@ public class LID implements ILID{
 
     private int slotNo;
 
+    /**
+     *
+     * @return slotNo
+     */
     public int getSlotNo(){
         return slotNo;
     }
 
     private PageId pageNo;
 
+    /**
+     *
+     * @return PageNo
+     */
     public PageId getPageNo(){
         if(pageNo == null)
         {
@@ -21,26 +29,34 @@ public class LID implements ILID{
         return pageNo;
     }
 
+    /**
+     * sets the PageNo
+     * @param pageNo contains PageId object to be assigned
+     */
     public void setPageNo(PageId pageNo) {
         this.pageNo = pageNo;
     }
 
+    /**
+     * sets the slot no
+     * @param slotNo contains slotNo to be assigned.
+     */
     public void setSlotNo(int slotNo) {
         this.slotNo = slotNo;
     }
 
-    ///<summary>
-    ///Default constructor
-    ///</summary>
+    /**
+     * Empty Constructor
+     */
     public LID() {  }
 
-    ///<summary>
-    ///Constructor of class
-    ///</summary>
+    /**
+     * parameterized constructor
+     * @param pageNo contains PageId to be assigned
+     * @param slotNo cotains slotNo to be assigned.
+     */
     public LID(PageId pageNo, int slotNo){
         this.pageNo = pageNo;
         this.slotNo = slotNo;
     }
-
-
 }
