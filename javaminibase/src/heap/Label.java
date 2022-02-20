c9e0d5b34b3da48eae7b240d3ba922eae783b4b2
Rahul Kumar <rahulrjjaiswal07@gmail.com>
2022-02-19 23:32:55 -0700

    Added changes to LHFPage.

--- javaminibase/src/heap/Label.java
+++ javaminibase/src/heap/Label.java
@@ -1,7 +1,33 @@
 package heap;
 
+import global.*;
+
 public class Label {
 
-    public Label(byte[] aLabel, int offset, int length) {
+    private ILID lid;
+
+    private Tuple tuple;
+
+    /**
+     * Sets the tuple member
+     * @param  tuple to be assigned
+     */
+    public void setTuple(Tuple tuple)
+    {
+        this.tuple = tuple;
     }
+
+    /**
+     * Sets the lid
+     * @param lid to be assigned
+     */
+    public void setLid(ILID lid){
+        this.lid.setPageNo(lid.getPageNo());
+        this.lid.setSlotNo(lid.getSlotNo());
+    }
+
+    /**
+     * Default constructor of class
+     */
+    public Label(){}
 }
