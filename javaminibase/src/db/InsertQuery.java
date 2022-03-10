ef3462132a802694eb6913e4ef73726ed5304f13
Shubham Bansal <illusionist.neo@gmail.com>
2022-03-09 23:01:38 -0700

    Small refactor

--- javaminibase/src/db/InsertQuery.java
+++ javaminibase/src/db/InsertQuery.java
@@ -1,47 +1,74 @@
 package db;
 
-public class InsertQuery implements IQuery {
+public class InsertQuery extends BaseQuery implements IQuery {
   private String dataFileName;
   private IndexOption indexOption;
-  private String dbName;
 
+  /**
+   * Public constructor
+   *
+   * @param _datafile
+   * @param _indexOption
+   * @param _dbname
+   */
   public InsertQuery(
       String _datafile,
       String _indexOption,
       String _dbname) {
+    super(_dbname);
     dataFileName = _datafile;
     indexOption = IndexOption.valueOf(_indexOption);
-    dbName = _dbname;
   }
 
+  /**
+   * get data file name
+   *
+   * @return  String data file name
+   */
   public String getDataFileName() {
     return dataFileName;
   }
 
+  /**
+   * set the data file name
+   *
+   * @param dataFileName String data file name
+   */
   public void setDataFileName(String dataFileName) {
     this.dataFileName = dataFileName;
   }
 
+  /**
+   * get indexOption
+   *
+   * @return  IndexOption
+   */
   public IndexOption getIndexOption() {
     return indexOption;
   }
 
+  /**
+   * set IndexOption from string
+   *
+   * @param indexOption String indexOption
+   */
   public void setIndexOption(String indexOption) {
     this.indexOption = IndexOption.valueOf(indexOption);
   }
 
+  /**
+   * set IndexOption
+   *
+   * @param indexOption IndexOption
+   */
   public void setIndexOption(IndexOption indexOption) {
     this.indexOption = indexOption;
   }
 
-  public String getDbName() {
-    return dbName;
-  }
-
-  public void setDbName(String dbName) {
-    this.dbName = dbName;
-  }
-
+  /**
+   * Execute the query
+   *
+   */
   public void execute() {
     // to be implemented
   }
