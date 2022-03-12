918da73bb1c4bf76699739e344635b87691d9b40
Sai Suchendhra <saisuchendhra@gmail.com>
2022-03-12 16:04:44 -0700

    Interface for index schemes

---
+++ javaminibase/src/diskmgr/IndexingSchemes/IndexSchemes.java
@@ -0,0 +1,19 @@
+package diskmgr.IndexingSchemes;
+
+public interface IndexSchemes {
+
+  //subject
+  public void createIndex();
+
+  //sub conf
+
+  //obj conf
+
+  //pred conf
+
+  //conf
+
+  //sub
+
+  //obj
+}
