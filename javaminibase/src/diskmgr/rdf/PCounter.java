7d0235f0cc26709a9faee101ae69f7c6b60e503a
Sai Suchendhra <saisuchendhra@gmail.com>
2022-03-19 14:06:33 -0700

    Changes PCounter class

--- javaminibase/src/diskmgr/rdf/PCounter.java
+++ javaminibase/src/diskmgr/rdf/PCounter.java
@@ -1,13 +1,12 @@
 package diskmgr.rdf;
-
-
-
 import global.GlobalConst;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -16,6 +15,16 @@ public class PCounter {
 
   public static int readCounter;
   public static int writeCounter;
+  public static String fileName=null;
+
+  public static String getFileName(){
+    fileName = Paths.get(
+        Paths.get(System.getProperty(GlobalConst.CURR_DIR_ENV)).toString(),
+        GlobalConst.ROOT_FOLDER,
+        "pCounter.json"
+    ).toString();
+    return fileName;
+  }
 
 
   /**
@@ -50,79 +59,153 @@ public class PCounter {
    * @throws IOException
    * @throws ParseException
    */
-  public static void write(String dbName, String qName) throws IOException, ParseException {
+  public static void writeToJSONFile(String dbName, String qName) throws IOException, ParseException {
 
-    String fileName = Paths.get(
-        Paths.get(System.getProperty(GlobalConst.CURR_DIR_ENV)).toString(),
-        GlobalConst.ROOT_FOLDER,
-        "pCounter.json"
-    ).toString();
 
+    fileName=getFileName();
     File newFile = new File(fileName);
 
     if (newFile.length() != 0) {
-      JSONParser parser = new JSONParser();
-      JSONObject obj = (JSONObject) parser.parse(
+      JSONParser jsonParser = new JSONParser();
+      JSONObject reportJsonObject = (JSONObject) jsonParser.parse(
           new FileReader(fileName));
 
-      Boolean isNewDBEntry = true;
-      for (Object keyStr : obj.keySet()) {
+      if (reportJsonObject.containsKey(dbName)) {
 
-        if (keyStr.equals(dbName)) {
-          isNewDBEntry = false;
-          JSONObject dbObj = (JSONObject) obj.get(keyStr);
-          Boolean isNewQuery = true;
-          for (Object queryKey : dbObj.keySet()) {
-            if (queryKey.equals(qName)) {
-              JSONObject queryObject = (JSONObject) dbObj.get(queryKey);
-              queryObject.put("Reads", readCounter );
-              queryObject.put("Writes", writeCounter );
-              isNewQuery = false;
-            }
-          }
-          if (isNewQuery == true) {
-            JSONObject queryObject = new JSONObject();
-            queryObject.put("Reads", readCounter);
-            queryObject.put("Writes", readCounter);
-            obj.put(qName, queryObject);
-            break;
-          }
+        JSONObject queryObj = (JSONObject) reportJsonObject.get(dbName);
+        if (queryObj.containsKey(qName)) {
+
+          JSONObject dataObj = (JSONObject)((JSONObject) reportJsonObject.get(dbName)).get(qName);
+          dataObj.put("Reads", readCounter);
+          dataObj.put("Writes", writeCounter);
+          queryObj.put(qName, dataObj);
+          reportJsonObject.put(dbName, queryObj);
+        } else {
+          JSONObject newQueryObj = new JSONObject();
+          JSONObject newDataObj = new JSONObject();
+
+          newDataObj.put("Reads", readCounter);
+          newDataObj.put("Writes", readCounter);
+          newQueryObj.put(qName, newDataObj);
+          reportJsonObject.put(dbName, newQueryObj);
         }
-      }
-      if (isNewDBEntry) {
-        JSONObject innerObj = new JSONObject();
-        JSONObject dataObj = new JSONObject();
-        dataObj.put("Reads", readCounter);
-        dataObj.put("Writes", writeCounter);
-        innerObj.put(qName, dataObj);
-        obj.put(dbName, innerObj);
-      }
-      try (FileWriter file = new FileWriter(fileName)) {
-        file.write(obj.toJSONString());
-        file.flush();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      System.out.println(obj);
+      } else {
 
-    } else {
-      JSONObject object = new JSONObject();
-      JSONObject querObject = new JSONObject();
-      JSONObject dataObject = new JSONObject();
-      dataObject.put("Reads", readCounter);
-      dataObject.put("Writes", writeCounter);
-      querObject.put(qName, dataObject);
-      object.put(dbName, querObject);
+        JSONObject newQueryObj = new JSONObject();
+        JSONObject newDataObj = new JSONObject();
+
+        newDataObj.put("Reads", readCounter);
+        newDataObj.put("Writes", readCounter);
+
+        newQueryObj.put(qName,newDataObj);
+        reportJsonObject.put(dbName, newQueryObj);
+      }
 
       try (FileWriter file = new FileWriter(fileName)) {
-        file.write(object.toJSONString());
+        file.write(reportJsonObject.toJSONString());
         file.flush();
       } catch (Exception e) {
         e.printStackTrace();
       }
 
     }
+    else{
+      JSONObject newReportObj = new JSONObject();
+      JSONObject newQueryObj = new JSONObject();
+      JSONObject newDataObj = new JSONObject();
+
+      newDataObj.put("Reads", readCounter);
+      newDataObj.put("Writes", readCounter);
+
+      newQueryObj.put(qName,newDataObj);
+      newReportObj.put(dbName, newQueryObj);
+
+      try (FileWriter file = new FileWriter(fileName)) {
+        file.write(newReportObj.toJSONString());
+        file.flush();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
 
 
   }
+
+  public static void printReadWriteCount(String dbName,String qName)
+      throws IOException, ParseException {
+    long rCount = -1;
+    long wCount = -1;
+
+    JSONParser jsonParser = new JSONParser();
+    JSONObject reportJsonObject = (JSONObject) jsonParser.parse(
+        new FileReader(fileName));
+
+    if(reportJsonObject.containsKey(dbName)){
+      JSONObject queryObject=(JSONObject) reportJsonObject.get(dbName);
+      if(queryObject.containsKey(qName)){
+        JSONObject dataObject=(JSONObject) queryObject.get(qName);
+        rCount=(long)dataObject.get("Reads");
+        wCount=(long)dataObject.get("Writes");
+
+        System.out.println("Reads : " + rCount+" Writes : " + wCount);
+      }
+      else{
+        System.out.println("Query Data not present");
+      }
+    }
+    else{
+      System.out.println("DB Data not present");
+    }
+
+
+  }
+
+  public static void printJsonFile() throws IOException, ParseException {
+    JSONParser jsonParser = new JSONParser();
+    JSONObject reportJsonObject = (JSONObject) jsonParser.parse(
+        new FileReader(fileName));
+    System.out.println(reportJsonObject.toJSONString());
+  }
+
+  public static void main(String args[]) throws IOException, ParseException {
+
+    writeToJSONFile("DBName1", "Query1");
+
+    readIncrement();
+    readIncrement();
+    writeIncrement();
+    writeToJSONFile("DBName2", "Query2");
+
+    readIncrement();
+    readIncrement();
+    writeIncrement();
+    writeIncrement();
+    writeToJSONFile("DBName3", "Query2");
+
+
+    readIncrement();
+    writeIncrement();
+
+    writeToJSONFile("DBName1", "Query1");
+
+    readIncrement();
+    readIncrement();
+    writeIncrement();
+    writeIncrement();
+    writeToJSONFile("DBName2", "Query2");
+
+    readIncrement();
+    readIncrement();
+    writeIncrement();
+    writeIncrement();
+    readIncrement();
+    writeToJSONFile("DBName3", "Query3");
+
+    printReadWriteCount("DBName3","Query3");
+    printJsonFile();
+
+
+
+  }
+
 }
